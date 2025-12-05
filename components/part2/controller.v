920ece725930072c2cd7107e9915e788b7d47e87
Lam Bui Bao <lambuibao@outlook.com>
2025-12-05 17:58:56 +0100

    i just realized what i did wrong

--- components/part2/controller.v
+++ components/part2/controller.v
@@ -1,5 +1,14 @@
-`include "../basics/instruction_memory.v"
-
-module controller ();
+module controller (
+    input [31:0] rdata,
+    output reg rf_we,
+    output reg sel_result,
+    output reg dmem_we,
+    output reg sel_alu_src_b,
+    output reg sel_ext
+);
+    // as used in instruction_memory, the rdata can be divided into their respective formats:
+    // where opcode is the first element of the array
+    wire[6:0] opcode;
+    assign opcode = rdata[6:0];
 
 endmodule
